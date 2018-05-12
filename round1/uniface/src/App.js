742b9c9b8a964e8eac3af56a9bdda7059cb4683c
c4h8 <panu.liesio@gmail.com>
2018-05-12 13:44:59 +0300

    complete assignment 1.7

--- round1/uniface/src/App.js
+++ round1/uniface/src/App.js
@@ -6,6 +6,26 @@ const Button = ({text, action}) => (
   </button>
 );
 
+const Statistic = ({identifier, value}) => (
+  <p>{identifier} {value}</p>
+)
+
+const Statistics = (props) => {
+  const {hyva, neutraali, huono} = props
+  const avg = ((hyva - huono) / (hyva + neutraali + huono)).toFixed(1)
+  const positivePerc = `${((100 * hyva) / (hyva + neutraali + huono)).toFixed(1)} %`
+
+  return (
+  <div>
+    <h2>statistiikka</h2>
+    <Statistic identifier="hyva" value={hyva} />
+    <Statistic identifier="neutraali" value={neutraali} />
+    <Statistic identifier="huono" value={huono} />
+    <Statistic identifier="keskiarvo" value={avg} />
+    <Statistic identifier="positiivisia" value={positivePerc} />
+  </div>);
+};
+
 class App extends React.Component {
   constructor() {
     super()
@@ -35,16 +55,16 @@ class App extends React.Component {
   }
 
   render() {
+    // const stats = Object.keys(this.state).map(key)
+    // console.log('entires: ', stats);
+
     return (
       <div className="App">
         <h2>anna palautetta</h2>
         <Button text={'hyva'} action={this.annaHyva} />
         <Button text={'neutraali'} action={this.annaNeutraali} />
         <Button text={'huono'} action={this.annaHuono} />
-        <h2>statistiikka</h2>
-        <p>hyva: {this.state.hyva}</p>
-        <p>neutraali: {this.state.neutraali}</p>
-        <p>huono: {this.state.huono}</p>
+        <Statistics {...this.state} />
       </div>
     );
   }
