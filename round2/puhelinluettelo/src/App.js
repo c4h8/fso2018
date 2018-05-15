ef3bc002f546d58c75a0a0490ac6c24644f7c6a6
c4h8 <panu.liesio@gmail.com>
2018-05-15 20:26:52 +0300

    complete assignment 2.6

---
+++ round2/puhelinluettelo/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Person = ({name}) => (
+  <p>{name}</p>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addData = (e) => {
+    e.preventDefault();
+    this.setState({
+      persons: this.state.persons.concat({name: this.state.newName}),
+      newName: ''
+    });
+  }
+
+  handleNameChange = (e) => {
+    e.preventDefault();
+    this.setState({newName: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addData}>
+          <div>
+            nimi:
+            <input
+              value={this.state.newName}
+              onChange={this.handleNameChange}
+            />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map((p, i) => <Person {...p} key={i}/> )}
+      </div>
+    )
+  }
+}
+
+export default App;
